1c8bd2a8a28a72330308aa1bfc4ec994b737f099
Paris26 <paris.gvr@gmail.com>
2024-10-10 00:11:40 +0300

    exercise 1

---
+++ askisi1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// TODO: function to take 3 strings as input
+void readStrings (string &s1, string &s2, string &s3) {
+  cout << "Enter 3 strings: ";
+  cin >> s1 >> s2 >> s3;
+}
+
+void comparestrings (string &s1, string &s2){
+  //apply mergse sort to sort the strings
+  mergesort(s1);
+  mergesort(s2);
+
+}
+
+void 
+
+int int main () {
+  //create 3 strings
+  string s1, s2, s3;
+
+     
+  
+  return 0;
+}
+  
